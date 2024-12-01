44433dabed7eef0eebf727eaabff02af03baad05
Alexa601 <akikoalexamma80@gmail.com>
2024-11-30 22:24:40 -0600

    principal admin

--- src/views/pprincipal.py
+++ src/views/pprincipal.py
@@ -7,27 +7,16 @@ class Principal(QMainWindow):
         super().__init__()
         uic.loadUi("src/views/admin/pprincipal.ui", self)  # Carga el diseño de pprincipal.ui
 
-        # Conectar botones a sus respectivas acciones
-        self.btnUsuarios_2.clicked.connect(self.inicio)
-        self.btnUsuarios.clicked.connect(self.usuarios)
+        # Asegúrate de conectar el botón después de cargar el diseño
         self.btnSalir.clicked.connect(self.cerrar_sesion)
 
-    def usuarios(self):
-        self.stackedWidget.setCurrentWidget(self.page_3)
-    def inicio(self):
-        """
-        Cambia a la página de Usuarios en el QStackedWidget
-        """
-        self.stackedWidget.setCurrentWidget(self.page)  # Cambia a la página de Usuarios
-
     def cerrar_sesion(self):
-        """
-        Cierra la ventana principal y vuelve al login.
-        """
-        # Cerrar la ventana principal
+        print("El botón SALIR fue presionado.")  # Debug
         self.close()
 
         # Crear una nueva instancia de la ventana de login y mostrarla
         from src.views.login import Login
         self.login = Login()
         self.login.login.show()
+
+
